e8a17d444711e0ef08761de6995f0e66360fec69
Babak Rahimi <babak82@gmail.com>
2025-02-03 20:10:59 +0100

    Create issue tracker app

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import { ThemeProvider } from "styled-components";
+import { Light } from "./theme/variables";
+import Home from "./components/Home";
+import IssueDetails from "./components/IssueDetails";
+import NotFound from "./components/404";
+
+function App() {
+  return (
+    <ThemeProvider theme={Light}>
+      <Wrapper>
+        <Header id="header" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/issue/:id" element={<IssueDetails />} />
+          <Route path="" element={<NotFound />} />
+        </Routes>
+      </Wrapper>
+    </ThemeProvider>
+  );
+}
+
+export default App;
